ef86c416471a63db9dc5fd78a88c03323e9063bc
bremxx <petr.kirienko@gmail.com>
2020-09-03 23:27:03 +0300

    скрипт для попапов

---
+++ source/js/form-submit.js
@@ -0,0 +1,39 @@
+const reviewForm = document.querySelector('.review-form'),
+  popup = document.querySelector('.popup'),
+  popupSuccess = document.querySelector('.popup--success'),
+  popupFailure = document.querySelector('.popup--failure'),
+  popupBtnSuccess = document.querySelector('.popup__button--success'),
+  popupBtnFailure = document.querySelector('.popup__button--failure');
+
+
+popup.classList.add('visually-hidden');
+reviewForm.noValidate = true;
+
+reviewForm.addEventListener('submit', event => {
+  event.preventDefault();
+  const form = event.target;
+  let validFields = 0;
+
+  for (let f = 0; f < form.elements.length; f++) {
+    let field = form.elements[f];
+
+    if (field.validity.valid) {
+      validFields++;
+      console.log(validFields);
+    }
+  }
+
+  if (validFields === form.elements.length) {
+    popupSuccess.classList.remove('visually-hidden');
+  } else {
+      popupFailure.classList.remove('visually-hidden');
+    }
+});
+
+popupBtnSuccess.addEventListener('click', () => {
+  popupSuccess.classList.add('visually-hidden');
+});
+
+popupBtnFailure.addEventListener('click', () => {
+  popupFailure.classList.add('visually-hidden');
+});
