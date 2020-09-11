f51df483798ae458508b7826e0f5b75a4e1faacb
bremxx <petr.kirienko@gmail.com>
2020-09-11 21:32:18 +0300

    привел код к единообразному написанию (js кавычки)

--- source/js/form-submit.js
+++ source/js/form-submit.js
@@ -1,17 +1,17 @@
-const reviewForm = document.querySelector('.review-form'),
-  popup = document.querySelector('.popup'),
-  popupSuccess = document.querySelector('.popup--success'),
-  popupFailure = document.querySelector('.popup--failure'),
-  popupBtnSuccess = document.querySelector('.popup__button--success'),
-  popupBtnFailure = document.querySelector('.popup__button--failure'),
-  formField = document.querySelector('.field');
+const reviewForm = document.querySelector(".review-form"),
+  popup = document.querySelector(".popup"),
+  popupSuccess = document.querySelector(".popup--success"),
+  popupFailure = document.querySelector(".popup--failure"),
+  popupBtnSuccess = document.querySelector(".popup__button--success"),
+  popupBtnFailure = document.querySelector(".popup__button--failure"),
+  formField = document.querySelector(".field");
 
 
 
-popup.classList.add('visually-hidden');
+popup.classList.add("visually-hidden");
 reviewForm.noValidate = true;
 
-reviewForm.addEventListener('submit', event => {
+reviewForm.addEventListener("submit", event => {
   event.preventDefault();
   const form = event.target;
   let validFields = 0;
@@ -21,23 +21,23 @@ reviewForm.addEventListener('submit', event => {
 
     if (field.validity.valid) {
       validFields++;
-      form.elements[f].classList.remove('field-invalid');
+      form.elements[f].classList.remove("field-invalid");
     } else{
-      form.elements[f].classList.add('field-invalid');
+      form.elements[f].classList.add("field-invalid");
     }
   }
 
   if (validFields === form.elements.length) {
-    popupSuccess.classList.remove('visually-hidden');
+    popupSuccess.classList.remove("visually-hidden");
   } else {
-      popupFailure.classList.remove('visually-hidden');
+      popupFailure.classList.remove("visually-hidden");
     }
 });
 
-popupBtnSuccess.addEventListener('click', () => {
-  popupSuccess.classList.add('visually-hidden');
+popupBtnSuccess.addEventListener("click", () => {
+  popupSuccess.classList.add("visually-hidden");
 });
 
-popupBtnFailure.addEventListener('click', () => {
-  popupFailure.classList.add('visually-hidden');
+popupBtnFailure.addEventListener("click", () => {
+  popupFailure.classList.add("visually-hidden");
 });
--- source/js/main-nav.js
+++ source/js/main-nav.js
@@ -1,21 +1,21 @@
-const navMain = document.querySelector('.main-nav');
-const navBtnOpen = document.querySelector('.button-burger');
-const navBtnClose = document.querySelector('.button-cross');
+const navMain = document.querySelector(".main-nav");
+const navBtnOpen = document.querySelector(".button-burger");
+const navBtnClose = document.querySelector(".button-cross");
 
-navMain.classList.remove('main-nav--nojs');
+navMain.classList.remove("main-nav--nojs");
 
-navBtnOpen.addEventListener('click', function() {
-  if (navMain.classList.contains('main-nav--closed')) {
-    navMain.classList.remove('main-nav--closed');
-    navMain.classList.add('main-nav--opened');
-    navBtnOpen.classList.add('visually-hidden');
+navBtnOpen.addEventListener("click", function() {
+  if (navMain.classList.contains("main-nav--closed")) {
+    navMain.classList.remove("main-nav--closed");
+    navMain.classList.add("main-nav--opened");
+    navBtnOpen.classList.add("visually-hidden");
   }
 });
 
-navBtnClose.addEventListener('click', function() {
-  if (navMain.classList.contains('main-nav--opened')) {
-    navMain.classList.remove('main-nav--opened');
-    navMain.classList.add('main-nav--closed');
-    navBtnOpen.classList.remove('visually-hidden');
+navBtnClose.addEventListener("click", function() {
+  if (navMain.classList.contains("main-nav--opened")) {
+    navMain.classList.remove("main-nav--opened");
+    navMain.classList.add("main-nav--closed");
+    navBtnOpen.classList.remove("visually-hidden");
   }
 });
